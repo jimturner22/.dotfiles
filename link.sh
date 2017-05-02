412fcec55ea4cd03c6577d22934fffdf1e664364
Jim Turner <jturner@opto22.com>
2017-05-02 11:47:47 -0700

    Add tmux config file

--- link.sh
+++ link.sh
@@ -6,9 +6,13 @@ export HOME_DIR=$(readlink --canonicalize ~)
 echo $PWD_DIR
 echo $HOME_DIR
 
+# Link the files and folders
+ln -s $PWD_DIR/.tmux.conf   $HOME_DIR/.tmux.conf
 ln -s $PWD_DIR/.vimrc       $HOME_DIR/.vimrc
 ln -s $PWD_DIR/.vim/colors/ $HOME_DIR/.vim/colors
 
+# Print out the result
+ls -la $HOME_DIR/.tmux.conf
 ls -la $HOME_DIR/.vimrc
 ls -la $HOME_DIR/.vim
 ls -la $HOME_DIR/.vim/colors/
